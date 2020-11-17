58f2b25061d033d572081323b7ab3ee1dffd5fcd
Sune Simonsen <sune@we-knowhow.dk>
2020-11-17 08:31:46 +0100

    Added a new Cache abstraction

---
+++ packages/store/src/Cache.js
@@ -0,0 +1,138 @@
+const updateIn = require("./updateIn");
+const isPathAffected = require("./isPathAffected");
+const getIn = require("./getIn");
+const normalizePath = require("./normalizePath");
+const Computed = require("./Computed");
+
+class Cache {
+  constructor(data = {}) {
+    this._data = data;
+    this.subscriptions = [];
+    this.scope = [];
+  }
+
+  _setData = (data) => {
+    this._data = data;
+  };
+
+  get data() {
+    return this._data;
+  }
+
+  scopePath(path) {
+    if (this.scope.length > 0) {
+      return [...this.scope, ...normalizePath(path)];
+    } else {
+      return normalizePath(path);
+    }
+  }
+
+  get(path) {
+    return getIn(this.data, this.scopePath(path));
+  }
+
+  set(...args) {
+    let path, value;
+
+    if (args.length === 1) {
+      path = [];
+      value = args[0];
+    } else {
+      path = args[0];
+      value = args[1];
+    }
+
+    this.update(path, () => value);
+
+    return this;
+  }
+
+  update(...args) {
+    let apply, path;
+
+    if (args.length === 1) {
+      path = [];
+      apply = args[0];
+    } else {
+      path = args[0];
+      apply = args[1];
+    }
+
+    path = this.scopePath(path);
+
+    this._setData(updateIn(this.data, path, (value) => apply(value, this)));
+    this.subscriptions.forEach((subscription) => {
+      if (isPathAffected(subscription.path, path)) {
+        subscription.listener(getIn(this.data, subscription.path), this);
+      }
+    });
+
+    return this;
+  }
+
+  subscribe(...args) {
+    let path, listener;
+
+    if (args.length === 1) {
+      path = [];
+      listener = args[0];
+    } else {
+      path = args[0];
+      listener = args[1];
+    }
+
+    const subscription = {
+      path: this.scopePath(path),
+      listener,
+      unsubscribe: () => {
+        this.subscriptions = this.subscriptions.filter(
+          (s) => s != subscription
+        );
+      },
+    };
+
+    this.subscriptions.push(subscription);
+
+    return subscription;
+  }
+
+  scoped(scope) {
+    const scopePath = normalizePath(scope);
+
+    if (scopePath.length === 0) {
+      return this;
+    }
+
+    const scopedStore = Object.create(this);
+    scopedStore.scope = [...scopedStore.scope, ...scopePath];
+    return scopedStore;
+  }
+
+  waitFor(...args) {
+    let path, predicate;
+
+    if (args.length === 1) {
+      path = [];
+      predicate = args[0];
+    } else {
+      path = args[0];
+      predicate = args[1];
+    }
+
+    return new Promise((resolve) => {
+      const initialValue = this.get(path);
+      if (predicate(initialValue)) {
+        resolve(initialValue);
+      }
+
+      const subscription = this.subscribe(path, (value) => {
+        if (predicate(value)) {
+          subscription.unsubscribe();
+          resolve(value);
+        }
+      });
+    });
+  }
+}
+
+module.exports = Cache;
---
+++ packages/store/src/Cache.spec.js
@@ -0,0 +1,337 @@
+const expect = require("unexpected").clone().use(require("unexpected-sinon"));
+const sinon = require("sinon");
+const Cache = require("./Cache");
+
+describe("Cache", () => {
+  describe("constructor", () => {
+    describe("when given no arguments", () => {
+      it("creates an empty cache", () => {
+        const cache = new Cache();
+        expect(cache.get(), "to equal", {});
+      });
+    });
+
+    describe("when given an object", () => {
+      it("creates a cache with the given value as the data", () => {
+        const cache = new Cache({
+          global: { testing: "The state" },
+        });
+
+        expect(cache.get(), "to equal", {
+          global: { testing: "The state" },
+        });
+      });
+    });
+  });
+
+  describe("set", () => {
+    it("sets the value at the given path", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      });
+
+      cache.set(["global", "testing"], "Hello beautiful world");
+
+      expect(cache.get(), "to equal", {
+        global: { testing: "Hello beautiful world" },
+      });
+    });
+
+    describe("when the path is just a string", () => {
+      it("use the path as the first segment", () => {
+        const cache = new Cache({
+          global: { testing: "Hello world" },
+        });
+
+        cache.set("global", "Hello beautiful world");
+
+        expect(cache.get(), "to equal", {
+          global: "Hello beautiful world",
+        });
+      });
+    });
+
+    describe("when the given path doesn't exist", () => {
+      it("creates the path", () => {
+        const cache = new Cache({
+          global: { testing: "The state" },
+        });
+
+        cache.set(["global", "new path"], "This is new");
+
+        expect(cache.get(), "to equal", {
+          global: {
+            testing: "The state",
+            "new path": "This is new",
+          },
+        });
+      });
+    });
+  });
+
+  describe("update", () => {
+    it("updates the value at the given path", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      });
+
+      cache.update(["global", "testing"], (v) => v.toUpperCase());
+
+      expect(cache.get(), "to equal", {
+        global: { testing: "HELLO WORLD" },
+      });
+    });
+
+    describe("when the given path doesn't exist", () => {
+      it("creates the path", () => {
+        const cache = new Cache({
+          global: { testing: "The state" },
+        });
+
+        cache.update(["global", "new path"], () => "This is new");
+
+        expect(cache.get(), "to equal", {
+          global: {
+            testing: "The state",
+            "new path": "This is new",
+          },
+        });
+      });
+    });
+  });
+
+  describe("subscribe", () => {
+    let cache, spy;
+
+    beforeEach(() => {
+      cache = new Cache({
+        global: {
+          numbers: [1, 2, 3],
+          sum: 6,
+        },
+      });
+
+      spy = sinon.spy();
+    });
+
+    describe("when only given a listener", () => {
+      it("subscribes to changes of the entire cache", () => {
+        cache.subscribe(spy);
+
+        cache.update(["global"], ({ numbers, sum }) => ({
+          numbers: [...numbers, 4],
+          sum: sum + 4,
+        }));
+
+        expect(cache.get(), "to equal", {
+          global: {
+            numbers: [1, 2, 3, 4],
+            sum: 10,
+          },
+        });
+
+        expect(spy, "to have calls satisfying", () => {
+          spy(
+            {
+              global: {
+                numbers: [1, 2, 3, 4],
+                sum: 10,
+              },
+            },
+            expect.it("to be a", Cache)
+          );
+        });
+      });
+    });
+
+    describe("when given a path and a listener", () => {
+      it("subscribes to the given path", () => {
+        cache.subscribe(["global", "numbers"], spy);
+
+        cache.update(["global"], ({ numbers, sum }) => ({
+          numbers: [...numbers, 4],
+          sum: sum + 4,
+        }));
+
+        expect(cache.get(), "to equal", {
+          global: {
+            numbers: [1, 2, 3, 4],
+            sum: 10,
+          },
+        });
+
+        expect(spy, "to have calls satisfying", () => {
+          spy([1, 2, 3, 4], expect.it("to be a", Cache));
+        });
+      });
+    });
+
+    it("doesn't fire if the subscribe path is not affected", () => {
+      cache.subscribe(["global", "numbers"], spy);
+
+      const average = (numbers) =>
+        numbers.reduce((a, b) => a + b, 0) / numbers.length;
+
+      cache.update(["global", "average"], (_, cache) =>
+        average(cache.get(["global", "numbers"]))
+      );
+
+      expect(cache.get(), "to equal", {
+        global: {
+          numbers: [1, 2, 3],
+          sum: 6,
+          average: 2,
+        },
+      });
+
+      expect(spy, "was not called");
+    });
+  });
+
+  describe("unsubscribe", () => {
+    it("unsubscribes a subscription", () => {
+      const cache = new Cache({
+        global: {
+          numbers: [1, 2, 3],
+          sum: 6,
+        },
+      });
+
+      const spy = sinon.spy();
+
+      const subscription = cache.subscribe(["global", "numbers"], spy);
+
+      cache.update(["global"], ({ numbers, sum }) => ({
+        numbers: [...numbers, 4],
+        sum: sum + 4,
+      }));
+
+      subscription.unsubscribe();
+
+      cache.update(["global"], ({ numbers, sum }) => ({
+        numbers: numbers.map((v) => v + 1),
+        sum: sum + numbers.length,
+      }));
+
+      expect(cache.get(), "to equal", {
+        global: {
+          numbers: [2, 3, 4, 5],
+          sum: 14,
+        },
+      });
+
+      expect(spy, "to have calls satisfying", () => {
+        spy([1, 2, 3, 4], expect.it("to be a", Cache));
+      });
+    });
+  });
+
+  describe("scoped", () => {
+    it("returns a scoped cache", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      }).scoped("global");
+
+      cache.update("testing", (v) => v.toUpperCase());
+
+      expect(cache.get("testing"), "to equal", "HELLO WORLD");
+    });
+
+    it("supports subscribe", () => {
+      const spy = sinon.spy();
+
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      }).scoped("global");
+
+      cache.subscribe("testing", spy);
+
+      cache.set("testing", "Hello beautiful world");
+
+      expect(cache.get("testing"), "to equal", "Hello beautiful world");
+
+      expect(spy, "to have calls satisfying", () => {
+        spy("Hello beautiful world", expect.it("to be a", Cache));
+      });
+    });
+
+    it("supports get", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      }).scoped("global");
+
+      cache.update("testing", (v) => v.toUpperCase());
+
+      expect(cache.get("testing"), "to equal", "HELLO WORLD");
+    });
+
+    it("supports set", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      }).scoped("global");
+
+      cache.set("testing", "Hello beautiful world");
+
+      expect(cache.get("testing"), "to equal", "Hello beautiful world");
+    });
+
+    it("supports update", () => {
+      const cache = new Cache({
+        global: { testing: "Hello world" },
+      }).scoped("global");
+
+      cache.update("testing", (v) => v.toUpperCase());
+
+      expect(cache.get("testing"), "to equal", "HELLO WORLD");
+    });
+
+    it("supports dispatch", () => {});
+
+    it("supports scoping a scoped cache", () => {
+      const cache = new Cache({
+        level1: { level2: { level3: "Hello world" } },
+      })
+        .scoped("level1")
+        .scoped("level2");
+
+      cache.update("level3", (v) => v.toUpperCase());
+
+      expect(cache.get("level3"), "to equal", "HELLO WORLD");
+    });
+  });
+
+  describe("waitFor", () => {
+    describe("when only given a predicate", () => {
+      it("returns a promise for when the given condition is true for the entire cache", async () => {
+        const cache = new Cache({
+          global: { status: "loading" },
+        });
+
+        setTimeout(() => {
+          cache.set(["global", "status"], "ready");
+        }, 1);
+
+        await cache.waitFor((data) => data.global.status === "ready");
+      });
+    });
+
+    describe("when given a path", () => {
+      it("returns a promise for when the given condition is true for that path", async () => {
+        const cache = new Cache({
+          global: { status: "loading" },
+        });
+
+        setTimeout(() => {
+          cache.set(["global", "status"], "ready");
+        }, 1);
+
+        const value = await cache.waitFor(
+          ["global", "status"],
+          (status) => status === "ready"
+        );
+
+        expect(value, "to equal", "ready");
+      });
+    });
+  });
+});
