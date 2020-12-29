6ab4c997fbd6311b9ad907b5e4782bf800ef6a09
Sune Simonsen <sune@we-knowhow.dk>
2020-12-29 09:53:42 +0100

    Remove @depository/preact-hooks, use @depository/preact/hooks instead

--- packages/preact-hooks/src/index.js
+++
@@ -1,6 +0,0 @@
-import { StoreContext } from "./StoreContext.js";
-export { useObservable } from "./useObservable.js";
-export { useInstanceId } from "./useInstanceId.js";
-export { useDispatch } from "./useDispatch.js";
-
-export const StoreProvider = StoreContext.Provider;
