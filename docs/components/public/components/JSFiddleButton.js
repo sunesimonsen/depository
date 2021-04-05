db2a41d2007293726997a6f2e919e2222e8a2f58
Sune Simonsen <sune@we-knowhow.dk>
2021-04-05 16:24:18 +0200

    Update the JSFiddle component versions

--- docs/components/public/components/JSFiddleButton.js
+++ docs/components/public/components/JSFiddleButton.js
@@ -14,10 +14,10 @@ const importmap = `
 {
   "imports": {
     "stylewars": "https://unpkg.com/stylewars@1.7.1/dist/bundle.esm.js",
-    "@depository/store": "https://unpkg.com/@depository/store@0.29.0/dist/store.esm.js",
-    "@depository/view": "https://unpkg.com/@depository/view@0.29.0/dist/view.esm.js",
-    "@depository/components/icons/": "https://unpkg.com/@depository/components@0.29.0/src/icons/",
-    "@depository/components": "https://unpkg.com/@depository/components@0.29.0/dist/components.esm.js"
+    "@depository/store": "https://unpkg.com/@depository/store@0.30.0/dist/store.esm.js",
+    "@depository/view": "https://unpkg.com/@depository/view@0.30.0/dist/view.esm.js",
+    "@depository/components/icons/": "https://unpkg.com/@depository/components@0.30.0/src/icons/",
+    "@depository/components": "https://unpkg.com/@depository/components@0.30.0/dist/components.esm.js"
   }
 }
 </script>
