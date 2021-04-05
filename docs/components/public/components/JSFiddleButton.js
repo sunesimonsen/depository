d17c5772b2e79ce7c81bfc0512155f58feacf220
Sune Simonsen <sune@we-knowhow.dk>
2021-04-05 23:57:20 +0200

    Run examples against the latest version of depository

--- docs/components/public/components/JSFiddleButton.js
+++ docs/components/public/components/JSFiddleButton.js
@@ -14,10 +14,10 @@ const importmap = `
 {
   "imports": {
     "stylewars": "https://unpkg.com/stylewars@1.9.0/dist/bundle.esm.js",
-    "@depository/store": "https://unpkg.com/@depository/store@0.30.0/dist/store.esm.js",
-    "@depository/view": "https://unpkg.com/@depository/view@0.30.0/dist/view.esm.js",
-    "@depository/components/icons/": "https://unpkg.com/@depository/components@0.30.0/src/icons/",
-    "@depository/components": "https://unpkg.com/@depository/components@0.30.0/dist/components.esm.js"
+    "@depository/store": "https://unpkg.com/@depository/store/dist/store.esm.js",
+    "@depository/view": "https://unpkg.com/@depository/view/dist/view.esm.js",
+    "@depository/components/icons/": "https://unpkg.com/@depository/components/src/icons/",
+    "@depository/components": "https://unpkg.com/@depository/components/dist/components.esm.js"
   }
 }
 </script>
