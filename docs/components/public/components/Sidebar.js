91a9fb7c2eb0c4d45f152cbb2714835e49f576e2
Sune Simonsen <sune@we-knowhow.dk>
2021-06-16 21:25:50 +0200

    Styleguide: Refactor sidebar

--- docs/components/public/components/Sidebar.js
+++ docs/components/public/components/Sidebar.js
@@ -2,18 +2,22 @@ import { html } from "@depository/view";
 import { css } from "stylewars";
 import { PageReference } from "./PageReference.js";
 
-const styles = css`
-  & {
-    padding: 16px;
-  }
-`;
-
 const listStyles = css`
   & {
     padding: 0;
     margin: 0;
   }
 `;
+
+export class List {
+  render({ children }) {
+    return html`
+      <ul class=${listStyles}>
+        ${children}
+      </ul>
+    `;
+  }
+}
 
 const listItemStyles = css`
   & {
@@ -23,51 +27,37 @@ const listItemStyles = css`
   }
 `;
 
+export class ListItem {
+  render({ children }) {
+    return html`<li class=${listItemStyles}>${children}</li>`;
+  }
+}
+
+const sidebarStyles = css`
+  & {
+    padding: 16px;
+  }
+`;
+
 export class Sidebar {
   render() {
     return html`
-      <nav class=${styles}>
-        <ul class=${listStyles}>
-          <li class=${listItemStyles}>
-            <${PageReference} id="anchor">Anchor<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="button">Button<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="iconbutton">Icon button<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="columnlayout">Column layout<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="borderlayout">Border layout<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="center">Center<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="spinner">Spinner<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="skeleton">Skeleton<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="pulse">Pulse<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="popup">Popup<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="menu">Menu<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="icons">Icons<//>
-          </li>
-          <li class=${listItemStyles}>
-            <${PageReference} id="textinput">TextInput<//>
-          </li>
-        </ul>
+      <nav class=${sidebarStyles}>
+        <${List}>
+          <${ListItem}><${PageReference} id="anchor">Anchor<//><//>
+          <${ListItem}><${PageReference} id="button">Button<//><//>
+          <${ListItem}><${PageReference} id="iconbutton">Icon button<//><//>
+          <${ListItem}><${PageReference} id="columnlayout">Column layout<//><//>
+          <${ListItem}><${PageReference} id="borderlayout">Border layout<//><//>
+          <${ListItem}><${PageReference} id="center">Center<//><//>
+          <${ListItem}><${PageReference} id="spinner">Spinner<//><//>
+          <${ListItem}><${PageReference} id="skeleton">Skeleton<//><//>
+          <${ListItem}><${PageReference} id="pulse">Pulse<//><//>
+          <${ListItem}><${PageReference} id="popup">Popup<//><//>
+          <${ListItem}><${PageReference} id="menu">Menu<//><//>
+          <${ListItem}><${PageReference} id="icons">Icons<//><//>
+          <${ListItem}><${PageReference} id="textinput">TextInput<//><//>
+        <//>
       </nav>
     `;
   }
