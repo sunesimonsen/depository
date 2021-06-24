27f58dafa019a7a760351c6eeb2bceb07b9adc92
Sune Simonsen <sune@we-knowhow.dk>
2021-06-24 19:23:29 +0200

    Document Center stretching

---
+++ docs/components/public/pages/center/Stretched.js
@@ -0,0 +1,23 @@
+import { html } from "@depository/view";
+import { css } from "stylewars";
+import { Center, ColumnLayout } from "@depository/components";
+
+const containerStyles = css`
+  & {
+    border: thin solid gray;
+    height: 100px;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+`;
+
+export default class Example {
+  render() {
+    return html`
+      <${ColumnLayout} columns="2" stretched>
+        <${Center} class=${containerStyles}>Not stretched<//>
+        <${Center} stretched class=${containerStyles}>Stretched<//>
+      <//>
+    `;
+  }
+}
--- docs/components/public/pages/center/index.js
+++ docs/components/public/pages/center/index.js
@@ -1,6 +1,15 @@
 import { html } from "@depository/view";
-import { Title, SubTitle, Line, Example } from "@depository/styleguide";
+
+import {
+  Title,
+  SubTitle,
+  Heading,
+  Line,
+  Example,
+} from "@depository/styleguide";
+
 import DefaultCenter from "./DefaultCenter.js";
+import Stretched from "./Stretched.js";
 
 class ColumnLayoutPage {
   render() {
@@ -8,8 +17,15 @@ class ColumnLayoutPage {
       <${Title}>Center<//>
       <${SubTitle}>Centering its children using CSS flex layout.<//>
       <${Line} />
+      <${Heading} level="2">How to use it<//>
+      <${Heading} level="3">Default<//>
       <${Example} src=${new URL("./DefaultCenter.js", import.meta.url)}>
         <${DefaultCenter} />
+      <//>
+      <${Heading} level="3">Stretched<//>
+      <p>The center layout can stretch to fill the surrouding container.</p>
+      <${Example} src=${new URL("./Stretched.js", import.meta.url)}>
+        <${Stretched} />
       <//>
     `;
   }
