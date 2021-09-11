bc126166a35115658e97988bf7273d2a8e7d8c1a
Sune Simonsen <sune@we-knowhow.dk>
2021-09-11 17:03:24 +0200

    Updated the importmaps

--- docs/components/public/importmap.js
+++ docs/components/public/importmap.js
@@ -1,9 +1,9 @@
 export const importmap = `{
   "imports": {
     "stylewars": "https://unpkg.com/stylewars@^1.9.0/dist/bundle.esm.js",
-    "@depository/store": "https://unpkg.com/@depository/store@^0.47.0/dist/store.esm.js",
-    "@depository/view": "https://unpkg.com/@depository/view@^0.47.0/dist/view.esm.js",
-    "@depository/components/icons/": "https://unpkg.com/@depository/components@^0.47.0/src/icons/",
-    "@depository/components": "https://unpkg.com/@depository/components@^0.47.0/dist/components.esm.js"
+    "@depository/store": "https://unpkg.com/@depository/store@^0.49.0/dist/store.esm.js",
+    "@depository/view": "https://unpkg.com/@depository/view@^0.49.0/dist/view.esm.js",
+    "@depository/components/icons/": "https://unpkg.com/@depository/components@^0.49.0/src/icons/",
+    "@depository/components": "https://unpkg.com/@depository/components@^0.49.0/dist/components.esm.js"
   }
 }`;
