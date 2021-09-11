61a081c094d119ebcb4aad269104dcc6ba2cac39
Sune Simonsen <sune@we-knowhow.dk>
2021-09-11 17:17:48 +0200

    Make Vite hotloading and devtools work together

--- examples/todomvc-react/public/index.js
+++ examples/todomvc-react/public/index.js
@@ -2,3 +2,9 @@ import { html, render } from "@depository/react/html";
 import { Application } from "./components/Application.js";
 
 render(html`<${Application} />`, document.getElementById("app"));
+
+if (import.meta.hot && window.__DEPOSITORY__) {
+  import.meta.hot.on("vite:beforeFullReload", () => {
+    window.__DEPOSITORY__.prepareReload();
+  });
+}
