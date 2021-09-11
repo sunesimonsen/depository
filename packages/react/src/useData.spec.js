1461537814bf2b0f4081a55104382f2518107b8e
Sune Simonsen <sune@we-knowhow.dk>
2021-09-11 17:54:59 +0200

    Make the react hooks top-level

--- packages/react/src/useData.spec.js
+++ packages/react/src/useData.spec.js
@@ -4,7 +4,7 @@ import unexpectedReaction from "unexpected-reaction";
 import { Store } from "@depository/store";
 import { functionMiddleware } from "@depository/function-middleware";
 import { StoreProvider, useData, useDispatch } from "./index.js";
-import { html } from "../html.js";
+import { html } from "./html.js";
 
 const simulate = unexpectedReaction.simulate;
 const mount = unexpectedReaction.mount;
