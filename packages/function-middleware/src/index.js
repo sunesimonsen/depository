04c49a195ba4446810e9609a8eac81e665afb4f5
Sune Simonsen <sune@we-knowhow.dk>
2021-10-27 10:42:49 +0200

    Formatted files

--- packages/function-middleware/src/index.js
+++ packages/function-middleware/src/index.js
@@ -3,28 +3,26 @@ const isPromise = (obj) =>
   (typeof obj === "object" || typeof obj === "function") &&
   typeof obj.then === "function";
 
-export const functionMiddleware = (...args) => async ({
-  store,
-  next,
-  action,
-}) => {
-  if (!action.payload || typeof action.payload !== "function") {
-    return next(action);
-  }
+export const functionMiddleware =
+  (...args) =>
+  async ({ store, next, action }) => {
+    if (!action.payload || typeof action.payload !== "function") {
+      return next(action);
+    }
 
-  const inputs = action.inputs && store.cache.get(action.inputs);
+    const inputs = action.inputs && store.cache.get(action.inputs);
 
-  let payload = inputs
-    ? action.payload(inputs, ...args)
-    : action.payload(...args);
+    let payload = inputs
+      ? action.payload(inputs, ...args)
+      : action.payload(...args);
 
-  if (isPromise(payload)) {
-    payload = await payload;
-  }
+    if (isPromise(payload)) {
+      payload = await payload;
+    }
 
-  return next({
-    ...action,
-    ...(inputs && { inputs }),
-    payload,
-  });
-};
+    return next({
+      ...action,
+      ...(inputs && { inputs }),
+      payload,
+    });
+  };
--- packages/status-middleware/src/index.js
+++ packages/status-middleware/src/index.js
@@ -3,32 +3,30 @@ const isPromise = (obj) =>
   (typeof obj === "object" || typeof obj === "function") &&
   typeof obj.then === "function";
 
-export const statusMiddleware = (...args) => async ({
-  store,
-  next,
-  action,
-}) => {
-  const execution = next(action);
-  const key = `statuses.${action.status}`;
+export const statusMiddleware =
+  (...args) =>
+  async ({ store, next, action }) => {
+    const execution = next(action);
+    const key = `statuses.${action.status}`;
 
-  if (isPromise(execution) && action.status) {
-    try {
-      store.cache.set(key, "loading");
-      store.cache.notify();
-      const result = await execution;
-      await next({
-        name: `Ready (${action.status})`,
-        payload: { [key]: "ready" },
-      });
-      return result;
-    } catch (e) {
-      await next({
-        name: `Failed (${action.status})`,
-        payload: { [key]: "failed" },
-      });
-      throw e;
+    if (isPromise(execution) && action.status) {
+      try {
+        store.cache.set(key, "loading");
+        store.cache.notify();
+        const result = await execution;
+        await next({
+          name: `Ready (${action.status})`,
+          payload: { [key]: "ready" },
+        });
+        return result;
+      } catch (e) {
+        await next({
+          name: `Failed (${action.status})`,
+          payload: { [key]: "failed" },
+        });
+        throw e;
+      }
+    } else {
+      return execution;
     }
-  } else {
-    return execution;
-  }
-};
+  };
